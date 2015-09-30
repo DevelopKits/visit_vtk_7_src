cb749dbd2aba3a911a9f8135a8989d96a04782e0
bonnell <bonnell@18c085ea-50e0-402c-830e-de6fd14e8384>
2015-09-30 15:27:52 +0000

    merge from trunk
    git-svn-id: svn+ssh://portal-auth.nersc.gov/project/projectdirs/visit/svn/visit/branches/bonnell/VTK-6.3-port/src@27321 18c085ea-50e0-402c-830e-de6fd14e8384

--- databases/FieldViewXDB/FieldViewXDB/avtFieldViewXDBOptions.C
+++ databases/FieldViewXDB/FieldViewXDB/avtFieldViewXDBOptions.C
@@ -36,73 +36,56 @@
 *
 *****************************************************************************/
 
-#ifndef PP_Z_STSD_FILE_FORMAT_H
-#define PP_Z_STSD_FILE_FORMAT_H
-#include <avtSTSDFileFormat.h>
-#include <PP_ZFileReader.h>
+// ************************************************************************* //
+//                              avtFieldViewXDBOptions.C
+// ************************************************************************* //
 
-class avtFileFormatInterface;
+#include <avtFieldViewXDBOptions.h>
+#include <DBOptionsAttributes.h>
+
+#include <string>
 
 // ****************************************************************************
-// Class: PP_Z_STSD_FileFormat
+//  Function: GetFieldViewXDBReadOptions
 //
-// Purpose:
-//   This file format uses
+//  Purpose:
+//      Creates the options for VTK readers.
 //
-// Notes:      
+//  Important Note:
+//      The code below sets up empty options.  If your format 
+//      does not require read options, no modifications are 
+//      necessary.
 //
-// Programmer: Brad Whitlock
-// Creation:   Tue Sep 16 15:27:25 PST 2003
+//  Programmer: Brad Whitlock
+//  Creation:   Mon Sep 28 16:08:26 PDT 2015
 //
-// Modifications:
-//   Brad Whitlock, Mon Oct 13 14:58:12 PST 2003
-//   Added methods that indicate that the cycle and time that the file format
-//   returns can be believed.
-//
-//   Brad Whitlock, Thu Sep 2 00:12:33 PDT 2004
-//   Added override for FreeUpResources.
-//
-//   Brad Whitlock, Mon Sep 15 16:24:23 PST 2008
-//   Added override for CanCacheVariable so variables read from this file
-//   format are never cached in the database. We already do our own low-cost
-//   caching.
-//
-//   Brad Whitlock, Fri Nov  7 10:24:22 PST 2008
-//   Added PDB method.
-//
-//   Mark C. Miller, Tue Apr 28 11:05:54 PDT 2009
-//   Changed name of PDB() to PDBfobj() to avoid symbol collision with PDB
-//   proper.
 // ****************************************************************************
 
-class PP_Z_STSD_FileFormat : public avtSTSDFileFormat
+DBOptionsAttributes *
+GetFieldViewXDBReadOptions(void)
 {
-public:
-    static avtFileFormatInterface *CreateInterface(PDBFileObject *pdb,
-         const char *const *filenames, int nList, int nBlocks);
+    DBOptionsAttributes *rv = new DBOptionsAttributes;
+    return rv;
+}
 
-    PP_Z_STSD_FileFormat(const char *filename);
-    PP_Z_STSD_FileFormat(PDBFileObject *p);
-    virtual ~PP_Z_STSD_FileFormat();
 
-    // Mimic PDBReader interface.
-    bool Identify();
-    void SetOwnsPDBFile(bool);
-    PDBFileObject *PDBfobj();
+// ****************************************************************************
+//  Function: GetFieldViewXDBWriteOptions
+//
+//  Purpose:
+//      Creates the options for VTK writers.
+//
+//  Programmer: Brad Whitlock
+//  Creation:   Mon Sep 28 16:08:26 PDT 2015
+//
+//  Modifications:
+//
+// ****************************************************************************
 
-    // Methods overrides for an STSD file format.
-    virtual const char   *GetType();
-    virtual int           GetCycle(void);
-    virtual double        GetTime(void);
-    virtual void          PopulateDatabaseMetaData(avtDatabaseMetaData *);
-    virtual vtkDataSet   *GetMesh(const char *var);
-    virtual vtkDataArray *GetVar(const char *var);
-    virtual void         *GetAuxiliaryData(const char *var, const char *type,
-                                           void *args, DestructorFunction &);
-    virtual bool          CanCacheVariable(const char *) { return false; }
-    virtual void          FreeUpResources();
-protected:
-    PP_ZFileReader        reader;
-};
-
-#endif
+DBOptionsAttributes *
+GetFieldViewXDBWriteOptions(void)
+{
+    DBOptionsAttributes *rv = new DBOptionsAttributes;
+    rv->SetBool("Strip mesh name prefix", false);
+    return rv;
+}
